0f1b4fd9e131e928ef7f6812e80f58b7be1e895c
Alexander Chelovekov <achelove@cisco.com>
2021-09-21 15:29:59 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container-horizontal">
+  <div class="container-vertical">
+    <div v-for="room in complianceData" :key="room.room">
+        <button @click="toggleRoomDevicesIsVisible(room)">{{ room.room }}</button>
+    </div>
+  </div>
+  <div class="container-vertical">
+    <div v-for="room in complianceData" :key="room.room">
+      <div v-for="device in room.devices" :key="device.hostname">
+        <button @click="toggleItemIsVisible(device)" v-if="room.isVisible">{{ device.hostname }}</button>
+      </div>
+    </div>
+  </div> 
+</div>
+<div>
+  <div v-for="room in complianceData" :key="room.room">
+    <div v-for="device in room.devices" :key="device.hostname">
+      <div :class="{ 'overlay-visible': device.isVisible, 'overlay-hidden': !device.isVisible}" @click="toggleItemIsVisible(device)">
+        <pre style="margin-left: 10rem;">{{ device.metricsValues }}</pre>
+      </div>
+    </div>
+  </div>
+</div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+      data: function() {
+        return { 
+          complianceData: [{room:"1.2",isVisible:false,devices:[{hostname:"S1-AG-01",isVisible:false,role:"AG",featureset:"BGP-EVPN",chassis:"N9K-C9504",linecards:["N9K-X9732C-EX","N9K-X9732C-EX","N9K-X9732C-EX","N9K-X9732C-EX"],metricsValues:[{metric:"svi",originalValue:"0",referenceValue:"1000",loadValue:"0.0",loadLevel:"Ok"},{metric:"mac",originalValue:"15474",referenceValue:"64000",loadValue:"0.24",loadLevel:"Ok"}]},{hostname:"S1-Leaf-04",isVisible:false,role:"AC",featureset:"BGP-EVPN",chassis:"N9K-C93180YC-FX",linecards:["N9K-C93180YC-FX"],metricsValues:[{metric:"svi",originalValue:"27",referenceValue:"2000",loadValue:"0.01",loadLevel:"Ok"},{metric:"mac",originalValue:"95000",referenceValue:"90000",loadValue:"1.06",loadLevel:"Critical"}]}]},{room:"1.4",isVisible:false,devices:[{hostname:"S1-AG-02",isVisible:false,role:"AG",featureset:"BGP-EVPN",chassis:"N9K-C9504",linecards:["N9K-X9732C-EX","N9K-X9732C-EX","N9K-X9732C-EX","N9K-X9732C-EX"],metricsValues:[{metric:"svi",originalValue:"0",referenceValue:"1000",loadValue:"0.0",loadLevel:"Ok"},{metric:"mac",originalValue:"15474",referenceValue:"64000",loadValue:"0.24",loadLevel:"Ok"}]},{hostname:"S2-Leaf-04",isVisible:false,role:"AC",featureset:"BGP-EVPN",chassis:"N9K-C93180YC-FX",linecards:["N9K-C93180YC-FX"],metricsValues:[{metric:"svi",originalValue:"27",referenceValue:"2000",loadValue:"0.01",loadLevel:"Ok"},{metric:"mac",originalValue:"95000",referenceValue:"90000",loadValue:"1.06",loadLevel:"Critical"}]}]}]
+        }
+      },
+    methods: {
+      toggleRoomDevicesIsVisible(item) {
+        this.complianceData.forEach(item => item.isVisible = false)
+        item.isVisible = true
+      },
+      toggleItemIsVisible(item) {
+        item.isVisible = !item.isVisible
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 0.5rem
+}
+
+.container-horizontal {
+  display: flex;
+  flex-direction: row;
+}
+
+.container-vertical {
+  display: flex;
+  flex-direction: column;
+}
+
+.overlay-visible {
+  position: fixed; /* Sit on top of the page content */
+  display: block;
+  width: 100%; /* Full width (cover the whole page) */
+  height: 100%; /* Full height (cover the whole page) */
+  top: 0; 
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+  z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+  cursor: pointer; /* Add a pointer on hover */
+}
+
+.overlay-hidden {
+  position: fixed; /* Sit on top of the page content */
+  display: none; /* Hidden by default */
+  width: 100%; /* Full width (cover the whole page) */
+  height: 100%; /* Full height (cover the whole page) */
+  top: 0; 
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+  z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+  cursor: pointer; /* Add a pointer on hover */
 }
 </style>
---
+++ src/components/HostData.vue
@@ -0,0 +1,23 @@
+<template>
+<button @click="toggleDetails">{{ hostData.hostname }}</button>
+<pre v-if="hostDataVisible"> 
+    <pre v-for="metric in hostData" :key="metric"> {{ metric}} </pre>
+</pre>
+</template>
+
+<script>
+export default {
+    props: ['hostData'],
+    data() {
+        return {
+            hostDataVisible: false
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.hostDataVisible = !this.hostDataVisible
+            console.log(this.hostDataVisible)
+        }
+    }
+}
+</script>
